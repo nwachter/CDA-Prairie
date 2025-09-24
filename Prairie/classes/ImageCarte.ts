de37f8969673bbb9a0b7c9414daab9cc9279c0df
nwachter <malaikaninaw@yahoo.fr>
2025-09-24 18:29:29 +0200

    wrong file prairie ts

--- Prairie/classes/ImageCarte.ts
+++ Prairie/classes/ImageCarte.ts
@@ -1,20 +1,29 @@
-import ImageCarte from "./ImageCarte";
+export default class ImageCarte {
+  public nbrColumn: number;
+  public nbrRow: number;
+  // Utiliser element.getBoundingClientRect()
+  public formatCarte: DOMRect;
+  public cartePositionInitX: number;
+  public cartePositionInitY: number;
+  public carteLargeur: number;
+  public carteHauteur: number;
+  public uniteX: number;
+  public uniteY: number;
 
-export default class Element{
-  public x: number;
-  public y: number;
-  public avatar: HTMLElement;
-  public carte: ImageCarte;
-
-  constructor(avatar: HTMLElement, carte: ImageCarte) {
-    this.x = 0;
-    this.y = 0;
-    this.avatar = avatar;
-    this.carte = carte;
+  constructor(nbrColumn: number, nbrRow: number, formatCarte: DOMRect) {
+    this.nbrColumn = nbrColumn;
+    this.nbrRow = nbrRow;
+    this.formatCarte = formatCarte;
+    this.carteHauteur = formatCarte.height;
+    this.carteLargeur = formatCarte.width;
+    this.uniteX = this.carteLargeur / nbrColumn;
+    this.uniteY = this.carteHauteur / nbrRow;
+    // pour positionner au centre d'une case
+    this.cartePositionInitX = formatCarte.left + this.uniteX / 2;
+    this.cartePositionInitY = formatCarte.top + this.uniteY / 2;
   }
 
-  public move(x: number, y: number) {
-    this.avatar.style.top = String(x * this.carte.uniteX + "px");
-    this.avatar.style.left = String(y + this.carte.uniteY + "px");
-  }
+  //   nbrColumn = 40;
+  //   nbrRow = 32;
+  //   formatCarte = carte.getBoundingClientRect();
 }
