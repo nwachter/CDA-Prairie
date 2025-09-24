827abb1619145e8434130244f8fc64de7f3909db
yannou64 <yannickpro64@gmail.com>
2025-09-24 22:22:57 +0200

    Ajustement front + configuration tsconfig ajusté

--- Prairie/front/prairie.ts
+++ Prairie/front/prairie.ts
@@ -1,59 +1,38 @@
-// const arthur = document.querySelector("#arthur");
-// const carte = document.querySelector("#carte");
-// const labyrinthe = document.querySelector("#labyrinthe");
-// const camelot = document.querySelector("#camelot");
-
-// // la grille fait 20 / 20 dont le 0
-// const nbrColumn = 20;
-// const nbrRow = 20;
-// const largeurCarte = carte.clientWidth;
-// const hauteurCarte = carte.clientHeight;
-
-// function arthurPosition(x, y) {
-//   const arthurX = (largeurCarte / nbrColumn) * x;
-//   const arthurY = (hauteurCarte / nbrRow) * y;
-//   arthur.style.left = arthurX + "px";
-//   arthur.style.top = arthurY + "px";
-// }
-
-// function labyrinthePosition(x, y) {
-//   const labyrintheX = (largeurCarte / nbrColumn) * x;
-//   const labyrintheY = (hauteurCarte / nbrRow) * y;
-//   labyrinthe.style.left = labyrintheX + "px";
-//   labyrinthe.style.top = labyrintheY + "px";
-// }
-
-// function camelotPosition(x, y) {
-//   const camelotX = (largeurCarte / nbrColumn) * x;
-//   const camelotY = (hauteurCarte / nbrRow) * y;
-//   camelot.style.left = camelotX + "px";
-//   camelot.style.top = camelotY + "px";
-// }
-
-// function arthurMove(x, y){
-//     // la position de arthur
-   
-// }
-// // position de départ
-// arthurPosition(4, 2);
-// labyrinthePosition(5, 5);
-// camelotPosition(10, 10);
-
-// // déplacement
-
-
-import Element from "../classes/Element" 
-import ImageCarte from "../classes/ImageCarte";
+import Element from "../classes/Element.js";
+import ImageCarte from "../classes/ImageCarte.js";
 
 // On dessine la carte du navigateur
-const imageCarte = new ImageCarte(40, 32, document.querySelector("#carte").getBoundingClientRect());
+const colonnes = 40;
+const rows = 32;
+const imageCarte = new ImageCarte(colonnes, rows, document.querySelector("#carte").getBoundingClientRect());
 
 // On initialise les élements avec la carte
-const arthur = new Element(document.querySelector("#arthur"), imageCarte)
+const arthur = new Element(document.querySelector("#arthur"), imageCarte);
 const labyrinthe = new Element(document.querySelector("#labyrinthe"), imageCarte);
 const camelot = new Element(document.querySelector("#camelot"), imageCarte);
 
-// position de départ
+// Générer aléatoirement emplacement de de camelot et arthur
+const randomX = Math.floor(Math.random() * colonnes);
+const randomY = Math.floor(Math.random() * rows);
+camelot.move(randomX, randomY);
+arthur.move(randomX, randomY);
 
+// Emplacement du labyrinthe
+labyrinthe.move(30, 26)
 
-// déplacement
+// Attente que user appui sur button et déplacement de arthur vers le labyrinthe
+const button_go = document.querySelector("#button_go") as HTMLElement;
+button_go.addEventListener("click", handleButtonGo);
+const button_reset = document.querySelector("#button_reset") as HTMLElement;
+button_reset.addEventListener("click", handleButtonReset);
+
+function handleButtonReset() {
+  location.reload();
+}
+function handleButtonGo() {
+  button_go.style.display = "none";
+  button_reset.style.display = "flex";
+}
+
+// reste à importer la fonction qui gère l'algorithme est qui renvoie une liste de coordonnée 
+
